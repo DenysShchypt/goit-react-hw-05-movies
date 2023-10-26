975038e2fbccca63967c257eaf28b0c9a05d0db6
DenysShchypt <denysshchyptsovit@gmail.com>
2023-10-26 22:48:11 +0300

    skilet

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 export default function NotFound(){
   return (
-    <div>Sorry, page not found. Please return to home page</div>
+    <div>Sorry, page not found. Please return to home page!</div>
   )
 }
 
